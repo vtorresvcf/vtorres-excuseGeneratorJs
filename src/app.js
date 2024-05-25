8abe8969c3604145900351555321761e4d2e0b13
Vicente Torres <vtorresvcf@gmail.com>
2024-05-25 19:22:48 +0200

    proyecto finalizado

--- src/app.js
+++ src/app.js
@@ -7,5 +7,26 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  function randomExcuse() {
+    let who = ["The dog", "My grandma", "The mailman", "My bird"];
+    let action = ["ate", "peed", "crushed", "broke"];
+    let what = ["my homework", "my phone", "the car"];
+    let when = [
+      "before the class",
+      "when I was sleeping",
+      "while I was exercising",
+      "during my lunch",
+      "while I was praying"
+    ];
+
+    let whonumRandom = Math.floor(Math.random() * who.length);
+    let actionnumRandom = Math.floor(Math.random() * action.length);
+    let whatnumRandom = Math.floor(Math.random() * what.length);
+    let whenumRandom = Math.floor(Math.random() * when.length);
+
+    let textExcuse = `${who[whonumRandom]} ${action[actionnumRandom]} ${what[whatnumRandom]} ${when[whenumRandom]}   `;
+
+    return textExcuse;
+  }
+  document.getElementById("excuse").innerHTML = randomExcuse();
 };
